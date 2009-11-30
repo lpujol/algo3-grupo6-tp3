f20e0b066e019306c0aea7ff391d4f90e981d8f3
jicalcagno <jicalcagno@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-11-30 18:17:31 +0000

    Le saqué el nivel a Juego y se lo dí a Laberinto. Ahora se deberian poder cargar niveles cusomizados. Fijense el archivo "nivel.txt", tienen que poner el ancho y el alto en las primeras dos lineas. No lo probe asi que no prometo nada.

--- src/modelo/CargadorDeNiveles.java
+++ src/modelo/CargadorDeNiveles.java
@@ -11,61 +11,50 @@ public class CargadorDeNiveles {
 	
 	Juego juego;
 	Laberinto laberinto;
-	int nivelSiguiente;
-	int cantidadDeNiveles;
 	
 	public CargadorDeNiveles(Juego juego, Laberinto laberinto) {
 		this.juego = juego;
 		this.laberinto = laberinto;
-		this.nivelSiguiente = 1;
-		this.cantidadDeNiveles = getCantidadDeNiveles();
-		
 	}
 	
-	public Nivel getSiguiente() {
+	private Posicion pasarAPosicionGlobal(Posicion posicionVieja) {
+		Posicion posicionGlobal;
+		int x;
+		int y;
+		x = posicionVieja.getX() * Laberinto.getTamanoDelBloque()
+					- Laberinto.getTamanoDelBloque() / 2;
+		y = posicionVieja.getY() * Laberinto.getTamanoDelBloque()
+					- Laberinto.getTamanoDelBloque() / 2;
+		posicionGlobal = new Posicion(x, y);
+		return posicionGlobal;
+	}
+	
+	public Nivel cargar(Nivel nivel, String rutaNivel) {
 		int x;
 		int y;
 		int caracter;
-		Nivel nivel = new Nivel(nivelSiguiente);
 		BufferedReader input = null;
-		File nivelFile = new File("nivel"+nivelSiguiente+".txt");
+		File nivelFile = new File(rutaNivel);
 		try {
 			input = new BufferedReader(new FileReader(nivelFile));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for (y = 1; y <= 31; y++) {
-			for (x = 1; x <= 28; x++) {
-				try {
+		try {
+			laberinto.setAnchoEnBloques(Integer.parseInt(input.readLine()));
+			laberinto.setAltoEnBloques(Integer.parseInt(input.readLine()));
+			for (y = 1; y <= laberinto.getAltoEnBloques(); y++) {
+				for (x = 1; x <= laberinto.getAnchoEnBloques(); x++) {
 					caracter = input.read();
-					agregarObjeto(caracter, nivel, new Posicion(x, y));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					agregarObjeto(caracter, nivel, pasarAPosicionGlobal(new Posicion(x, y)));
 				}
-			}
-			try {
 				input.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
         return nivel;
-	}
-	
-	public boolean haySiguiente() {
-		return this.nivelSiguiente <= this.cantidadDeNiveles; 
-	}
-	
-	private int getCantidadDeNiveles() {
-		int i = 1;
-		File nivelFile = new File("nivel"+i+".txt");
-		while (nivelFile.exists()) {
-			i++;
-			nivelFile = new File("nivel"+i+".txt");
-		}
-		return i-1;
+        
 	}
 
 	private void agregarObjeto(int caracter, Nivel nivel, Posicion posicion) {
