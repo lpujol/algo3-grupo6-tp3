f2458ff038350c0cbf05d7a6f678757d31772d2f
MatiasJA <MatiasJA@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-03 09:29:54 +0000

    -Arreglado el movimiento de los fantasmas, ya no se quedan indecisos en un rincon. -Nueva prueva para fantasmas -Faltan agregar comentarios al codigo. -Existe un bug del pacman cuando es comido desde atras.

--- trunk/src/modeloTests/FantasmaCelesteTest.java
+++ trunk/src/modeloTests/FantasmaCelesteTest.java
@@ -2,7 +2,9 @@ package modeloTests;
 
 import modelo.Fantasma;
 import modelo.FantasmaCeleste;
+import modelo.Juego;
 import modelo.Laberinto;
+import modelo.Pacman;
 import modelo.Posicion;
 import junit.framework.TestCase;
 
@@ -24,23 +26,63 @@ public class FantasmaCelesteTest extends TestCase {
 	
 	public void testMovimiento(){
 		Laberinto miLaberinto=new Laberinto(null);
-		Fantasma miFantasma=new FantasmaCeleste(152,312,miLaberinto,4);
-		miLaberinto.getNivel().agregarFantasma(miFantasma);
-		miFantasma.vivir();				
+		Fantasma unFantasma=new FantasmaCeleste(152,312,miLaberinto,4);
+		miLaberinto.getNivel().agregarFantasma(unFantasma);
+		unFantasma.vivir();				
 		Posicion posicionSiguiente=new Posicion(152,316);
+		assertEquals(posicionSiguiente.getX(),unFantasma.getPosicion().getX());
+		assertEquals(posicionSiguiente.getY(),unFantasma.getPosicion().getY());
+		unFantasma.vivir();
+		unFantasma.vivir();
+		unFantasma.vivir();
+		posicionSiguiente=new Posicion(152,328);
+		assertEquals(posicionSiguiente.getX(),unFantasma.getPosicion().getX());
+		assertEquals(posicionSiguiente.getY(),unFantasma.getPosicion().getY());
+		
+	};
+
+	
+	public void testPersecucion(){
+        Juego miJuego = new Juego();        
+        Pacman miPacman= miJuego.getLaberinto().getPacman();		
+        miJuego.getLaberinto().getNivel().agregarFantasma(new FantasmaCeleste(200,56,miJuego.getLaberinto(),4));
+		Fantasma miFantasma=miJuego.getLaberinto().getFantasmas().get(0);				
+		miFantasma.vivir();	
+		miPacman.vivir();
+		miFantasma.vivir();
+		miPacman.vivir();
+		miFantasma.vivir();
+		miPacman.vivir();
+		miFantasma.vivir();
+		miPacman.vivir();
+		Posicion posicionSiguiente=new Posicion(200,72);
+		assertEquals(posicionSiguiente.getX(),miFantasma.getPosicion().getX());
+		assertEquals(posicionSiguiente.getY(),miFantasma.getPosicion().getY());
+		for(int i = 0; i < 108; i++){
+			miFantasma.vivir();
+			miPacman.vivir();}		
+		posicionSiguiente=new Posicion(296,312);
+		assertEquals(posicionSiguiente.getX(),miFantasma.getPosicion().getX());
+		assertEquals(posicionSiguiente.getY(),miFantasma.getPosicion().getY());
+		for(int i = 0; i < 4; i++){
+			miFantasma.vivir();
+			miPacman.vivir();}		
+		posicionSiguiente=new Posicion(296,328);
+		assertEquals(posicionSiguiente.getX(),miFantasma.getPosicion().getX());
+		assertEquals(posicionSiguiente.getY(),miFantasma.getPosicion().getY());	
+		for(int i = 0; i < 12; i++){
+			miFantasma.vivir();
+			miPacman.vivir();}		
+		posicionSiguiente=new Posicion(344,328);
 		assertEquals(posicionSiguiente.getX(),miFantasma.getPosicion().getX());
 		assertEquals(posicionSiguiente.getY(),miFantasma.getPosicion().getY());
 		miFantasma.vivir();
-		miFantasma.vivir();
-		miFantasma.vivir();
-		posicionSiguiente=new Posicion(152,328);
+		miPacman.vivir();		
+		posicionSiguiente=new Posicion(344,332);
 		assertEquals(posicionSiguiente.getX(),miFantasma.getPosicion().getX());
 		assertEquals(posicionSiguiente.getY(),miFantasma.getPosicion().getY());
 		
 	};
-	
-	
-	
 	
 	
 }
