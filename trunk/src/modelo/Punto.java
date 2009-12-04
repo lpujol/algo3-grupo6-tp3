0b89f935a81e7ea2029305135f69d66dfec16262
maucarp960 <maucarp960@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-04 15:16:24 +0000

    agregue  algunos  audios  necesarios. tuve problemas con el audio  de  cuando pacman se  come a  un  punto  es  decir cuando puse ese  codigo  en la clase  juego se  jugaba  de  manera  pausadaes  por  eso  que lo  puse  en la clase punto

--- trunk/src/modelo/Punto.java
+++ trunk/src/modelo/Punto.java
@@ -1,15 +1,31 @@
 package modelo;
 
-public class Punto extends Comestible {
+import vista.Audio;
 
+public class Punto extends Comestible {
+	
+    
+    
 	public Punto(Posicion posicion, Laberinto laberinto) {
 		super(posicion, laberinto);
+		//agregadoParaAudio
+		audioActivo=true;
+		//----
 	}
 
 	@Override
 	public void comer() {
+		
+		//agregadoAudio
+		if (audioActivo){
+		 		audio = new Audio("comePunto.wav");
+		 		audio.play(false);
+    	}
+		//----
+		
+		
 		this.comido = true;
-		this.laberinto.puntoComido();
+		this.laberinto.puntoComido(this);
 	}
 
 	@Override
