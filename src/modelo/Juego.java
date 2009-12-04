fb224de4518f68ceb542149ca9771f19441c26b1
maucarp960 <maucarp960@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-04 15:16:24 +0000

    agregue  algunos  audios  necesarios. tuve problemas con el audio  de  cuando pacman se  come a  un  punto  es  decir cuando puse ese  codigo  en la clase  juego se  jugaba  de  manera  pausadaes  por  eso  que lo  puse  en la clase punto

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -6,30 +6,42 @@ import java.util.TimerTask;
 
 import ar.uba.fi.algo3.titiritero.ObjetoVivo;
 
+import vista.Audio;
 import vista.VistaPunto;
 import vista.VistaPuntoPoder;
 
 
 public class Juego implements ObjetoVivo{
 
-
+//agregadoAudio
+	private Audio audio;
+	//---
 	private static final int VALOR_BONUS = 1000;
 	private static final int VALOR_PUNTO = 10;
 	private boolean puntoDePoderActivo;
 	private Laberinto laberinto;
 	private double puntosAcumulados;
 	private int cantidadVidas;
+	private ArrayList<VistaPunto> vistaPuntos;
+	private ArrayList<VistaPuntoPoder> vistaPuntosPoder;
 	private int contadorticks;
 
 	
 	
 	public Juego(){
+		//agregadoAudio
+		if (true){
+	 		audio = new Audio("inicio.au");
+	 		audio.play(false);
+	    }
+		//---
 
 		this.puntoDePoderActivo=false;
 		this.puntosAcumulados=0;
 		this.laberinto = new Laberinto(this);
 		this.cantidadVidas=laberinto.getNivel().getPacman().getCantidadVidas();
-		
+		vistaPuntos=new ArrayList<VistaPunto>();
+		vistaPuntosPoder=new ArrayList<VistaPuntoPoder>();
 	}	
 	
 	public void pasarNivel(){
@@ -67,6 +79,14 @@ public class Juego implements ObjetoVivo{
 	}
 
 	public void fantasmaComido(Fantasma fantasma) {
+		
+	//-----agregado para audio
+		
+		if (true){
+	 		audio = new Audio("muerteFantasma.au");
+	 		audio.play(false);
+	    }
+		//-----
 		fantasma.setEstrategiaActual(new EstrategiaIrACasa(fantasma));
 		
 	}
@@ -74,13 +94,27 @@ public class Juego implements ObjetoVivo{
 	
 
 
-	public void puntoDePoderComido() {
+	public void puntoDePoderComido(PuntoDePoder punto) {
 		this.puntoDePoderActivo=true;
 		
+		//agregado para audio
+		if (puntoDePoderActivo){
+	 		audio = new Audio("comePoder.wav");
+	 		audio.play(false);
+	    }
+		//--
+		
+		for(VistaPuntoPoder p:vistaPuntosPoder){
+			if(p.getPosicionable()==punto){
+				p.setNombreArchivoImagen("negro.jpg");
+			}
+		}
 		ArrayList<Fantasma> fantasmas=this.laberinto.getFantasmas();
 		for(Fantasma fantasma:fantasmas){
 			fantasma.setEstrategiaActual(new EstrategiaEscapar(fantasma));
 		}
+		
+		
 		Timer t=new Timer();
 		t.schedule(new TimerTask(){
 			@Override
@@ -91,9 +125,21 @@ public class Juego implements ObjetoVivo{
 		, 5000);
 		
 	}
+		/*
+		Thread t = new Thread(new EfectoPuntoDePoderTerminado() );
+		t.start();
+	}
 	
+	private class EfectoPuntoDePoderTerminado implements Runnable
+	{
+		
+		public void run()
+		{
+			Thread.sleep(5000);
+			
+		}
+	}*/
 	
-
 	public void efectoPuntoDePoderTerminado(){
 		this.puntoDePoderActivo=false;
 		ArrayList<Fantasma> fantasmas=this.laberinto.getFantasmas();
@@ -106,6 +152,16 @@ public class Juego implements ObjetoVivo{
 
 	public void pacmanComido() {
 		this.cantidadVidas --;
+		
+		
+		//agregadoAudio
+		if (this.cantidadVidas>0){
+	 		audio = new Audio("muerte.wav");
+	 		audio.play(false);
+	    }
+		//-----
+		
+		
 		if(this.cantidadVidas>0){
 			this.laberinto.getNivel().getPacman().disminuirVida();
 			this.laberinto.getNivel().enviarPacmanAPosicionInicial();
@@ -117,17 +173,41 @@ public class Juego implements ObjetoVivo{
 	}
 
 
-	public void puntoComido() {
+	public void puntoComido(Punto punto) {
+		
+		
 		puntosAcumulados=puntosAcumulados+VALOR_PUNTO*laberinto.getNivel().getNumero();
+		for(VistaPunto p:vistaPuntos)
+		{
+			if(p.getPosicionable()==punto)
+			{
+				p.setNombreArchivoImagen("negro.jpg");
+			}
+		}
 	}
 	
 
 	public void bonusComido() {
+		
+		//agregado para audio
+		if (true){
+	 		audio = new Audio("comePoder.wav");
+	 		audio.play(false);
+	    }
+		//--
+		
 		puntosAcumulados=puntosAcumulados+VALOR_BONUS*laberinto.getNivel().getNumero();
 		
 	}
-	
-	
+
+	public void vistaPuntos(ArrayList<VistaPunto> vistaPuntos) {
+		this.vistaPuntos=vistaPuntos;
+	}
+
+	public void vistaPuntosPoder(ArrayList<VistaPuntoPoder> vistaPuntosPoder) {
+		this.vistaPuntosPoder=vistaPuntosPoder;
+		
+	}
 
 	@Override
 	public void vivir() {
