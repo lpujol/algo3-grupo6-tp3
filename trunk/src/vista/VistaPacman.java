5b3ea1091bc7aa9d18698df09f18d65aa57748c9
lea87crzz <lea87crzz@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-05 02:50:06 +0000

    *agregue distintas imagenes al pacman segun para donde mire... hay que pulirlo un poco, pero la idea esta *saque lo relacionado con el audio de la clase juego y punto, ya que tiene que estar en una vista, habria que hacerlo
    nada mas

--- trunk/src/vista/VistaPacman.java
+++ trunk/src/vista/VistaPacman.java
@@ -3,8 +3,11 @@ package vista;
 import java.awt.Color;
 
 import modelo.Laberinto;
+import modelo.Pacman;
 
+import ar.uba.fi.algo3.titiritero.Dibujable;
 import ar.uba.fi.algo3.titiritero.Posicionable;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 import ar.uba.fi.algo3.titiritero.vista.Circulo;
 import ar.uba.fi.algo3.titiritero.vista.Imagen;
 /*
@@ -17,17 +20,59 @@ public class VistaPacman extends Circulo{
 
 }*/
 
-public class VistaPacman extends ImagenAnimada{
-	public VistaPacman(Posicionable pos){
-		super(pos);
-		Imagen img=new Imagen();
-		img.setNombreArchivoImagen("/vista/pacmanFrame1.jpg");
-		img.setPosicionable(pos);
-		this.add(img);
-		img=new Imagen();
-		img.setNombreArchivoImagen("/vista/pacmanFrame2.jpg");
-		img.setPosicionable(pos);
-		this.add(img);
-		this.setDelay(10);
+public class VistaPacman implements Dibujable{
+	ImagenAnimada derecha;
+	ImagenAnimada izquierda;
+	ImagenAnimada arriba;
+	ImagenAnimada abajo;
+	Pacman pacman;
+	
+	public VistaPacman(Pacman pos){
+		this.pacman=pos;
+		derecha=new ImagenAnimada(pos);
+		derecha.add("/vista/pacmanFrame1.jpg");
+		derecha.add("/vista/pacmanFrame2.jpg");
+		derecha.setDelay(10);
+		izquierda=new ImagenAnimada(pos);
+		izquierda.add("/vista/pacmanFrame1.jpg");
+		izquierda.add("/vista/pacmanFrame2Izq.jpg");
+		izquierda.setDelay(10);
+		arriba=new ImagenAnimada(pos);
+		arriba.add("/vista/pacmanFrame1.jpg");
+		arriba.add("/vista/pacmanFrame2Arriba.jpg");
+		arriba.setDelay(10);
+		abajo=new ImagenAnimada(pos);
+		abajo.add("/vista/pacmanFrame1.jpg");
+		abajo.add("/vista/pacmanFrame2Abajo.jpg");
+		abajo.setDelay(10);
+	}
+	@Override
+	public void dibujar(SuperficieDeDibujo superfice) {
+		switch (this.pacman.getDireccion()){
+		case Derecha:
+			derecha.dibujar(superfice);
+			break;
+		case Izquierda:
+			izquierda.dibujar(superfice);
+			break;
+		case Arriba:
+			arriba.dibujar(superfice);
+			break;
+		case Abajo:
+			abajo.dibujar(superfice);
+			break;
+			
+		
+		}
+		
+	}
+	@Override
+	public Posicionable getPosicionable() {
+		return pacman;
+	}
+	@Override
+	public void setPosicionable(Posicionable posicionable) {
+		this.pacman=(Pacman)posicionable;
+		
 	}
 }
