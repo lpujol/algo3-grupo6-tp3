9f2893fcbdf70ca0b01cdb609d90359ca11a9cfe
lea87crzz <lea87crzz@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-11 22:18:01 +0000

    agregue que muestre la cantidad de vidas... modifique posicion q implemente posicionable( 8-) ), para poder usarlo... osea q no cambie nada de Posicion

--- src/vista/VistaEstadoJuego.java
+++ src/vista/VistaEstadoJuego.java
@@ -2,14 +2,18 @@ package vista;
 
 import java.awt.Color;
 
+import titiriteroModificado.Imagen;
+
+import modelo.Posicion;
+
 
 import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
 import ar.uba.fi.algo3.titiritero.vista.Cuadrado;
-import ar.uba.fi.algo3.titiritero.vista.Imagen;
 
 public class VistaEstadoJuego extends Cuadrado {
 	
 	Imagen pacman;
+	private EstadoJuego estadoJuego;
 
 	public VistaEstadoJuego(EstadoJuego estado) {
 		super(estado.getAncho(),estado.getAlto());
@@ -17,14 +21,27 @@ public class VistaEstadoJuego extends Cuadrado {
 		this.setPosicionable(estado);
 		
 		pacman=new Imagen();
-		pacman.setNombreArchivoImagen("/vista/pacmanFrame1.jpg");
-		
+		pacman.setNombreArchivoImagen("/vista/pacmanFrame2_24.png");
+		this.estadoJuego=estado;
 		
 		// TODO Auto-generated constructor stub
 	}	
 	
 	public void dibujar(SuperficieDeDibujo superfice){
-		super.dibujar(superfice);		
+		super.dibujar(superfice);
+		Posicion pos=new Posicion(this.estadoJuego.getX()+10,this.estadoJuego.getY()+10);
+		if(estadoJuego.vidas()>0){
+			pacman.setPosicionable(pos);
+			pacman.dibujar(superfice);
+		}
+		if(estadoJuego.vidas()>1){
+			pos.avanzarHorizontal(25);
+			pacman.dibujar(superfice);
+		}
+		if(estadoJuego.vidas()>2){
+			pos.avanzarHorizontal(25);
+			pacman.dibujar(superfice);
+		}
 	}
 
 
