12999caacdc9fe81ae0e65fb82993dd397a498f2
lea87crzz <lea87crzz@b69dfce8-cfd4-11de-bb02-e302c5db49d5>
2009-12-15 11:47:11 +0000

    agregue el metodo clone a posicion, para hacer mas elegante cosas que haciamos de forma fea cambie que la estrategia ir a casa, lleve a los fantasmas a su posicion inicial, xq sino, venia sucediendo que una vez que el f rojo y el celeste se superponian no se separaban mas, ahora x lo menos cuando los matas, se separan

--- src/modelo/EstrategiaIrACasa.java
+++ src/modelo/EstrategiaIrACasa.java
@@ -11,12 +11,12 @@ public class EstrategiaIrACasa implements IEstrategia {
 	
 	
 	public Posicion getDestino() {
-		if(fantasma.getPosicion().equals(fantasma.getLaberinto().obtenerPosicionCasa())){
+		if(fantasma.getPosicion().equals(fantasma.getPosicionInicial())){//.getLaberinto().obtenerPosicionCasa())){
 			fantasma.setEstrategiaActual(new EstrategiaSalirDeCasa(fantasma));
 			fantasma.recuperarVelocidadInicial();
 			fantasma.estaVivo();
 		}
-		return fantasma.getLaberinto().obtenerPosicionCasa();		
+		return fantasma.getPosicionInicial();//.getLaberinto().obtenerPosicionCasa();		
 	}
 
 }
